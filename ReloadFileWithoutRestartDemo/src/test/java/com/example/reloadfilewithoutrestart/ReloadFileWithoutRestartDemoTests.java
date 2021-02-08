4f3b85e056f4cc0fb4909e617a6ba8dbad5111af
maheshd-md <maheshd@entrib.com>
2021-02-09 00:54:44 +0530

    Class name changed

--- ReloadFileWithoutRestartDemo/src/test/java/com/example/reloadfilewithoutrestart/ReloadFileWithoutRestartDemoTests.java
+++ ReloadFileWithoutRestartDemo/src/test/java/com/example/reloadfilewithoutrestart/ReloadFileWithoutRestartDemoTests.java
@@ -1,10 +1,10 @@
-package com.example.WorkFlowManagerDemo;
+package com.example.reloadfilewithoutrestart;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WorkFlowManagerDemoApplicationTests {
+class ReloadFileWithoutRestartDemoTests {
 
 	@Test
 	void contextLoads() {
